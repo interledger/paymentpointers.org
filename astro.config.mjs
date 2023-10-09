7d230dfe764060e2c3a4a2953e6da896aa59693f
Chen Hui Jing <1461498+huijing@users.noreply.github.com>
2023-10-09 16:54:06 +0800

    Update custom header implementation (#35)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
-import overrideIntegration from "./src/overrideIntegration.mjs";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://paymentpointers.org",
   integrations: [
-    overrideIntegration(),
     starlight({
       title: "Payment Pointers",
       description:
@@ -16,6 +13,9 @@ export default defineConfig({
         "./node_modules/@interledger/docs-design-system/src/styles/green-theme.css",
         "./node_modules/@interledger/docs-design-system/src/styles/ilf-docs.css",
       ],
+      components: {
+        Header: "./src/components/Header.astro",
+      },
       social: {
         github: "https://github.com/interledger/paymentpointers.org",
       },
@@ -30,9 +30,6 @@ export default defineConfig({
       ],
     }),
   ],
-
-  // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
-  image: { service: { entrypoint: "astro/assets/services/sharp" } },
   server: {
     port: 1102,
   },
