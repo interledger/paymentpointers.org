437679ec988627f92e67887256ab9849587ede80
JoblersTune <sarah@interledger.org>
2024-12-06 14:15:18 +0200

    feat: added basic site tracking using Umami

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,18 @@ export default defineConfig({
       title: "Payment Pointers",
       description:
         "Payment Pointers are a standardized identifier for payment accounts. In the same way that an email address provides an identifier for a mailbox in the email ecosystem a payment pointer is used by an account holder to share the details of their account with a counter-party.",
-      customCss: [
+        head: [
+          {
+            tag: 'script',
+            attrs: {
+              defer: true,
+              'data-website-id': 'cab4fd0a-e2db-4666-a974-5b199f0d6d43',
+              src: 'https://ilf-site-analytics.netlify.app/script.js',
+              'data-domains': 'paymentpointers.org'
+            }
+          }
+        ],
+        customCss: [
         "./node_modules/@interledger/docs-design-system/src/styles/teal-theme.css",
         "./node_modules/@interledger/docs-design-system/src/styles/ilf-docs.css",
       ],
