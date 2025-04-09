5b14c97d1cb2d10dac6b0e212ebdba762e5695c3
Chen Hui Jing <1461498+huijing@users.noreply.github.com>
2025-04-09 12:43:55 +0800

    chore: bump dependencies to latest versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,25 +10,25 @@ export default defineConfig({
       title: "Payment Pointers",
       description:
         "Payment Pointers are a standardized identifier for payment accounts. In the same way that an email address provides an identifier for a mailbox in the email ecosystem a payment pointer is used by an account holder to share the details of their account with a counter-party.",
-        head: [
-          {
-            tag: 'script',
-            attrs: {
-              defer: true,
-              'data-website-id': 'cab4fd0a-e2db-4666-a974-5b199f0d6d43',
-              src: 'https://ilf-site-analytics.netlify.app/script.js',
-              'data-domains': 'paymentpointers.org'
-            }
-          }
-        ],
-        customCss: [
+      head: [
+        {
+          tag: "script",
+          attrs: {
+            defer: true,
+            "data-website-id": "cab4fd0a-e2db-4666-a974-5b199f0d6d43",
+            src: "https://ilf-site-analytics.netlify.app/script.js",
+            "data-domains": "paymentpointers.org",
+          },
+        },
+      ],
+      customCss: [
         "./node_modules/@interledger/docs-design-system/src/styles/teal-theme.css",
         "./node_modules/@interledger/docs-design-system/src/styles/ilf-docs.css",
       ],
       plugins: [starlightLinksValidator()],
       components: {
         Header: "./src/components/Header.astro",
-        PageSidebar: './src/components/PageSidebar.astro'
+        PageSidebar: "./src/components/PageSidebar.astro",
       },
       expressiveCode: {
         styleOverrides: {
@@ -36,9 +36,7 @@ export default defineConfig({
           borderRadius: "var(--border-radius)",
         },
       },
-      social: {
-        github: "https://github.com/interledger/paymentpointers.org",
-      },
+      social: [{ icon: "github", label: "GitHub", href: "https://github.com/interledger/paymentpointers.org" }],
       sidebar: [
         { label: "Explainer", link: "/" },
         { label: "Design Goals", link: "/goals" },
